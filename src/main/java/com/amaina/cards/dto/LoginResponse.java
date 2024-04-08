516140ce060adc0a9a7886c691279bfd7837f1f1
amaina <maina.alex2010@gmail.com>
2024-04-08 18:46:00 +0300

    Update login response

--- src/main/java/com/amaina/cards/dto/LoginResponse.java
+++ src/main/java/com/amaina/cards/dto/LoginResponse.java
@@ -8,7 +8,7 @@ import lombok.Builder;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record LoginResponse(@JsonProperty("access_token") String accessToken,
                             @JsonProperty("token_type") String tokenType,
-                            @JsonProperty("expires_in") int expiresIn,
+                            @JsonProperty("expires_in") Integer expiresIn,
                             @JsonProperty("error") String error
 ) {
 }
